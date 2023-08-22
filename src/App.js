f38b74ef384881f5dcc1d52d5018447eed98e26f
Lina372 <polina.sv04@gmail.com>
2023-08-22 14:51:39 +0300

    версия 3

--- src/App.js
+++ src/App.js
@@ -6,14 +6,35 @@
 // import Login from './Login';
 // import Cookies from 'js-cookie';
 // import AdminPage from "./Pages/AdminPage";
+// import Appbar from "./Appbar";
 //
 // const App = () => {
 //     const [isLoggedIn, setIsLoggedIn] = useState(!!Cookies.get('token'));
+//     const [userRole, setUserRole] = useState(null);
 //
 //     useEffect(() => {
 //         const token = Cookies.get('token');
 //         setIsLoggedIn(!!token);
+//
+//         if (token) {
+//             const tokenPayload = token.split(".")[1];
+//             const decodedToken = JSON.parse(atob(tokenPayload));
+//             setUserRole(decodedToken.role);
+//         } else {
+//             setUserRole(null);
+//         }
 //     }, []);
+//
+//     useEffect(() => {
+//         if (isLoggedIn) {
+//             const token = Cookies.get('token');
+//             const tokenPayload = token.split(".")[1];
+//             const decodedToken = JSON.parse(atob(tokenPayload));
+//             setUserRole(decodedToken.role);
+//         } else {
+//             setUserRole(null);
+//         }
+//     }, [isLoggedIn]);
 //
 //     const handleLogin = () => {
 //         setIsLoggedIn(true);
@@ -22,20 +43,21 @@
 //     const handleLogout = () => {
 //         Cookies.remove('token');
 //         setIsLoggedIn(false);
+//         setUserRole(null);
 //     };
 //
 //     return (
 //         <Router>
+//             {isLoggedIn && <Appbar onLogout={handleLogout} userRole={userRole} />}
 //             <Routes>
 //                 <Route path="/" element={isLoggedIn ? <Home onLogout={handleLogout} /> : <Navigate to="/login" />} />
-//                 <Route path="/table" element={<TablePage onLogout={handleLogout} />} />
+//                 <Route path="/table" element={isLoggedIn ?<TablePage onLogout={handleLogout} /> : <Navigate to="/login" />} />
 //                 <Route
 //                     path="/login"
 //                     element={isLoggedIn ? <Navigate to="/" /> : <Login onLogin={handleLogin} />}
 //                 />
 //                 <Route path="/admin">
-//                     <Route path="/admin" element={<AdminPage onLogout={handleLogout} />} />
-//                     {/*<Route path="/admin" element={<AdminPage />} />*/}
+//                     <Route path="/admin" element={isLoggedIn ?<AdminPage userRole={userRole} onLogout={handleLogout} /> : <Navigate to="/login" />} />
 //                 </Route>
 //             </Routes>
 //         </Router>
@@ -45,6 +67,12 @@
 // export default App;
 //
 //
+//
+//
+//
+//
+
+
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Pages/Home';
@@ -65,25 +93,39 @@ const App = () => {
         if (token) {
             const tokenPayload = token.split(".")[1];
             const decodedToken = JSON.parse(atob(tokenPayload));
-            console.log("Decoded Token:", decodedToken);
             setUserRole(decodedToken.role);
         } else {
             setUserRole(null);
         }
+
+        // Установка интервала для проверки времени жизни токена каждые 30 секунд
+        const tokenExpirationCheckInterval = setInterval(() => {
+            checkTokenExpiration();
+        }, 30000); // 30 секунд
+
+        return () => {
+            // Очистка интервала при размонтировании компонента
+            clearInterval(tokenExpirationCheckInterval);
+        };
     }, []);
 
-    useEffect(() => {
-        if (isLoggedIn) {
-            // Обработка изменения токена и роли при входе
-            const token = Cookies.get('token');
+    // Функция для проверки времени жизни токена и удаления его
+    const checkTokenExpiration = () => {
+        const token = Cookies.get('token');
+        if (token) {
             const tokenPayload = token.split(".")[1];
             const decodedToken = JSON.parse(atob(tokenPayload));
-            setUserRole(decodedToken.role);
-        } else {
-            // Обработка изменения токена и роли при выходе
-            setUserRole(null);
+            const tokenExpiration = new Date(decodedToken.exp * 1000);
+            const currentTime = new Date();
+
+            if (currentTime > tokenExpiration) {
+                // Токен просрочен, удаляем его
+                Cookies.remove('token');
+                setIsLoggedIn(false);
+                setUserRole(null);
+            }
         }
-    }, [isLoggedIn]);
+    };
 
     const handleLogin = () => {
         setIsLoggedIn(true);
@@ -92,6 +134,7 @@ const App = () => {
     const handleLogout = () => {
         Cookies.remove('token');
         setIsLoggedIn(false);
+        setUserRole(null);
     };
 
     return (
@@ -99,13 +142,13 @@ const App = () => {
             {isLoggedIn && <Appbar onLogout={handleLogout} userRole={userRole} />}
             <Routes>
                 <Route path="/" element={isLoggedIn ? <Home onLogout={handleLogout} /> : <Navigate to="/login" />} />
-                <Route path="/table" element={<TablePage onLogout={handleLogout} />} />
+                <Route path="/table" element={isLoggedIn ? <TablePage onLogout={handleLogout} /> : <Navigate to="/login" />} />
                 <Route
                     path="/login"
                     element={isLoggedIn ? <Navigate to="/" /> : <Login onLogin={handleLogin} />}
                 />
                 <Route path="/admin">
-                    <Route path="/admin" element={<AdminPage userRole={userRole} onLogout={handleLogout} />} />
+                    <Route path="/admin" element={isLoggedIn ? <AdminPage userRole={userRole} onLogout={handleLogout} /> : <Navigate to="/login" />} />
                 </Route>
             </Routes>
         </Router>
@@ -114,69 +157,3 @@ const App = () => {
 
 export default App;
 
-//
-//
-// import React, { useState, useEffect } from 'react';
-// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-// import Home from './Pages/Home';
-// import TablePage from './Pages/TablePage';
-// import Login from './Login';
-// import Cookies from 'js-cookie';
-// import AdminPage from "./Pages/AdminPage";
-// import Appbar from './Appbar';
-//
-// const App = () => {
-//     const [isLoggedIn, setIsLoggedIn] = useState(!!Cookies.get('token'));
-//     const [userRole, setUserRole] = useState(null);
-//
-//     useEffect(() => {
-//         const token = Cookies.get('token');
-//         setIsLoggedIn(!!token);
-//
-//         if (token) {
-//             const tokenPayload = token.split(".")[1];
-//             const decodedToken = JSON.parse(atob(tokenPayload));
-//             console.log("Decoded Token:", decodedToken);
-//             setUserRole(decodedToken.role);
-//         } else {
-//             setUserRole(null);
-//         }
-//     }, []);
-//
-//     const handleLogin = () => {
-//         setIsLoggedIn(true);
-//     };
-//
-//     const handleLogout = () => {
-//         Cookies.remove('token');
-//         setIsLoggedIn(false);
-//         setUserRole(null);
-//     };
-//
-//     return (
-//         <Router>
-//
-//             {isLoggedIn && <Appbar onLogout={handleLogout} userRole={userRole} />}
-//
-//             <Routes>
-//                 {isLoggedIn ? (
-//                     <>
-//                         <Route path="/" element={<Home onLogout={handleLogout} />} />
-//                         <Route path="/table" element={<TablePage onLogout={handleLogout} />} />
-//                         <Route path="/admin">
-//                             <Route path="/admin" element={<AdminPage userRole={userRole} onLogout={handleLogout} />} />
-//                         </Route>
-//                     </>
-//                 ) : (
-//                     <Route path="/" element={<Navigate to="/login" />} />
-//                 )}
-//                 <Route
-//                     path="/login"
-//                     element={isLoggedIn ? <Navigate to="/" /> : <Login onLogin={handleLogin} />}
-//                 />
-//             </Routes>
-//         </Router>
-//     );
-// };
-//
-// export default App;
